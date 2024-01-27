536ff653f38622ac52b31c0283ef5d3316769304
Dave <88671265+shagat@users.noreply.github.com>
2024-01-27 20:34:43 +0530

    conditional content

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,26 @@ import { EXAMPLES } from "./data";
 import TabButton from "./components/TabButton/TabButton";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     setSelectedTopic(selectedButton);
-    console.log('current topic=>',selectedTopic);
   }
 
-  console.log('app component executed')
+  let tabContent = (<p>Please select a topic</p>);
+
+  if (selectedTopic) {
+    tabContent = (<div id="tab-content">
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>
+          {EXAMPLES[selectedTopic].code}
+        </code>
+      </pre>
+    </div>)
+  }
+
   return (
     <div>
       <Header />
@@ -39,15 +51,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>
-              {EXAMPLES[selectedTopic].code}
-              </code>
-            </pre>
-          </div>
+          {tabContent}
         </section>
       </main>
     </div>
--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,5 +1,4 @@
 export default function TabButton({ children, onSelect }) {
-  console.log('tab button executed')
   return (
     <li>
       <button onClick={onSelect}>{children}</button>
